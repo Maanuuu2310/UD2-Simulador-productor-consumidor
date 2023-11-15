a7a64d1ccffee7d2749437a6b1d2fd89d0dd5439
Manu <mgamez244@gmail.com>
2023-11-15 14:13:46 +0000

    feat: modificado el experimento del main

--- src/App.java
+++ src/App.java
@@ -7,8 +7,8 @@ public class App {
     public static void main(String[] args) {
         Huertito huertito = new Huertito();
 
-        Granjero granjero1 = new Granjero("Paco", 5, huertito, 3);
-        Cliente cliente1 = new Cliente("Faustino", 5, huertito, 3);
+        Granjero granjero1 = new Granjero("Paco", 5, huertito, 1);
+        Cliente cliente1 = new Cliente("Faustino", 5, huertito, 6);
         granjero1.start();
         cliente1.start();
     }
